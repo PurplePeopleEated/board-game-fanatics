e7b720e8f78cbeccda737e14b48d1e27a2acc538
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-04 21:15:09 -0500

    uncommented out required property

--- server.js
+++ server.js
@@ -23,13 +23,11 @@ const sess = {
   })
 };
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));})
