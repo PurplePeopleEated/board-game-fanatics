d0e4129d57c65976867a3a204c4a8879054a8e6a
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-04 19:28:49 -0500

    correction in parameters required in await function

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,12 +3,12 @@ const boardgame = require('../models/boardgame');
 
 //GET request for home routes
 router.get('/', async (req, res) => {
-    try{const boardgameData = await Project.findAll().catch((err) => { res.json(err);});
+    try{const boardgameData = await boardgame.findAll().catch((err) => { res.json(err);});
     const boardgame = boardgameData.map((boardgame) => boardgame.get({ plain: true }));
         res.render('homepage', { boardgame });} 
-    catch(err){res.status(500).json(err);};})
+    catch(err){console.error(err);res.status(500).json(err);};})
 
 router.get('/project/:id', async (req, res) => {
     try{const boardgameData = await boardgame.findByPk(req.params.id,{include:{model:User}});   
         boardgameData.get({plain: true});res.render('boardgame', { boardgameData });} 
-    catch(err){res.status(500).json(err);};})
+    catch(err){console.error(err);res.status(500).json(err);};})
