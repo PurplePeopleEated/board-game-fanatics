13c5dac9c7e8e6605528f94a9e164ea9d0feb941
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-04 22:24:58 -0500

    redefined router variable specific to home-routes for export purposes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,16 @@
-const router = require('express').Router();
+const homeRouter = require('express').Router();
 const boardgame = require('../models/boardgame');
 
 //GET request for home routes
-router.get('/', async (req, res) => {
+homeRouter.get('/', async (req, res) => {
     try{const boardgameData = await boardgame.findAll().catch((err) => { res.json(err);});
     const boardgame = boardgameData.map((boardgame) => boardgame.get({ plain: true }));
         res.render('homepage', { boardgame });} 
     catch(err){console.error(err);res.status(500).json(err);};})
 
-router.get('/project/:id', async (req, res) => {
+homeRouter.get('/project/:id', async (req, res) => {
     try{const boardgameData = await boardgame.findByPk(req.params.id,{include:{model:User}});   
         boardgameData.get({plain: true});res.render('boardgame', { boardgameData });} 
     catch(err){console.error(err);res.status(500).json(err);};})
+
+module.exports = homeRouter;    
