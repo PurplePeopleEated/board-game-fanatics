0ad02c0be2f3b00ea24a5702c8a8159e2e7bc20a
Karra <karra.maynard@gmail.com>
2023-12-05 20:07:19 -0500

    reference user_id

--- models/Boardgame.js
+++ models/Boardgame.js
@@ -26,7 +26,13 @@ timeToPlay: {
   type: DataTypes.INTEGER,
   allowNull: false
 },
-
+user_id: {
+  type: DataTypes.INTEGER,
+  references: {
+    model: 'user',
+    key: 'id'
+  }
+}
 },
 {
 sequelize,
