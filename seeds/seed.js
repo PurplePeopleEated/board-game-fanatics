056230b4c66a47ac34005a37c1a185643d434662
Karra <karra.maynard@gmail.com>
2023-12-06 18:48:58 -0500

    typo

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,7 +2,7 @@ const sequelize = require('../config/connection');
 const {User, Boardgame} = require ('../models');
 
 const userData = require('./userData.json');
-const gameData = require('./userData.json');
+const gameData = require('./gameData.json');
 
 const seedDB = async () => {
   await sequelize.sync({force: true});
