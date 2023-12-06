0e4ed9cc7aa7f72a6b76ea5fb83696a7baccbeee
Karra <karra.maynard@gmail.com>
2023-12-05 21:04:20 -0500

    sync with sequelize

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,7 +5,9 @@ const userData = require('./userData.json');
 const gameData = require('./userData.json');
 
 const seedDB = async () => {
-  
+  await sequelize.sync({force: true});
+
+  const users = await User.bulkCreate(userData);
 };
 
 seedDB();
