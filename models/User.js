d10498aa6542e6461c7bc4f56b65264bb27bb9ed
Karra <karra.maynard@gmail.com>
2023-12-05 19:48:19 -0500

    import User.js and attach sequelize

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  pass: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [8, 80] }}}, {
+
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  modelName: 'user'}
+);
+
+module.exports = User;
