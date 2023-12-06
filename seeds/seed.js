b842e69a29ef43609f7d3a689947fce7bcf24f2a
Karra <karra.maynard@gmail.com>
2023-12-05 21:06:13 -0500

    for loop to seed for amount of users in object

--- seeds/seed.js
+++ seeds/seed.js
@@ -8,6 +8,13 @@ const seedDB = async () => {
   await sequelize.sync({force: true});
 
   const users = await User.bulkCreate(userData);
+
+  for (const game of gameData) {
+    await Boardgame.create({
+      ...game,
+      user_id: users[Math.floor(math.random() * users.length)].id
+    });
+  } process.exit(0);
 };
 
 seedDB();
