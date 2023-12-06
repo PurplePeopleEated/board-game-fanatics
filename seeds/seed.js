61d2a7355d908126d8ad75b93431da1eb9a1ff02
Karra <karra.maynard@gmail.com>
2023-12-05 21:02:28 -0500

    create and call seedDB()

---
+++ seeds/seed.js
@@ -0,0 +1,11 @@
+const sequelize = require('../config/connection');
+const {User, Boardgame} = require ('../models');
+
+const userData = require('./userData.json');
+const gameData = require('./userData.json');
+
+const seedDB = async () => {
+  
+};
+
+seedDB();
