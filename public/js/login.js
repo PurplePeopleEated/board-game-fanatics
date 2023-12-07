cdc169723c05f6b536166303fe62156a38c0cf5b
Karra <karra.maynard@gmail.com>
2023-12-06 23:14:28 -0500

    clean, move signupHandler

--- public/js/login.js
+++ public/js/login.js
@@ -1,21 +1,3 @@
-const signupHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.getElementById('name-signup');
-  const email = document.getElementById('email-signup');
-  const pass = document.getElementById('pass-signup');
-
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({username, email, pass}),
-      headers: {'Content-Type:': 'application/json'}
-    });
-    
-    if (response.ok) {document.location.replace('/inventory');}
-    else {alert(response.statusText);}
-}};
-
 const loginHandler = async (event) => {
   event.preventDefault();
 
@@ -29,9 +11,8 @@ const loginHandler = async (event) => {
       headers: {'Content-Type': 'application/json'}
     });
     
-    if (response.ok) {document.location.replace('/inventory');}
-    else {alert(response.statusText);}
+    if (response.ok) {document.location.replace('/inventory');
+    } else {alert(response.statusText);}
   }};
 
-document.querySelector('.signup-form').addEventListener('submit', signupHandler);
 document.querySelector('.login-form').addEventListener('submit', loginHandler);
