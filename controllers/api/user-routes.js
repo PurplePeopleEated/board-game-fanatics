a8aa3ced182879e586ba89f530492633ce738fb3
Karra <karra.maynard@gmail.com>
2023-12-07 06:58:23 -0500

    comment

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -17,15 +17,18 @@ router.post('/login', async (req, res) => {
   // Find the user who matches the posted e-mail address
   try {
     const userData = await User.findOne({where: { email: req.body.email }});
-    
-    if(!userData) {res.status(400).json({message: 'Incorrect email or password, please try again'});
-    return;}
-    // Verify the posted password with the password store in the database
-    const validPassword = await userData.checkPassword(req.body.pass);
-    if (!validPassword) {res.status(400).json({message: 'Incorrect email or password, please try again'});
-    return;}
+    console.log(userData);
+    // if(!userData) {res.status(400).json({message: 'Incorrect email or password, please try again'});
+    //   return;}
+    // // Verify the posted password with the password store in the database
+    // const validPassword = await userData.checkPassword(req.body.pass);
+    // console.log(validPassword)
+    // if (!validPassword) {res.status(400).json({message: 'Incorrect email or password, please try again'});
+    //   return;}
+
     // Create session variables based on the logged in user
-    req.session.save(() => {req.session.user_id = userData.id;req.session.logged_in = true;
+     req.session.save(() => {req.session.user_id = userData.id;req.session.logged_in = true;
+      res.setHeader('Content-Type', 'application/json');
       res.json({user: userData, message: 'You are now logged in!'});});
     } catch(err) {res.status(400).json(err);}});
 
