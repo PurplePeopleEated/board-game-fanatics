1305e94e074224d2c980ba496356ecae5d2c9e4c
Karra <karra.maynard@gmail.com>
2023-12-06 23:16:03 -0500

    create signup.js

---
+++ public/js/signup.js
@@ -0,0 +1,19 @@
+const signupHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.getElementById('name-signup');
+  const email = document.getElementById('email-signup');
+  const pass = document.getElementById('pass-signup');
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({username, email, pass}),
+      headers: {'Content-Type:': 'application/json'}
+    });
+    
+    if (response.ok) {document.location.replace('/inventory');
+    } else {alert(response.statusText);}
+}};
+
+document.querySelector('.signup-form').addEventListener('submit', signupHandler);
