c4d78f7e5989374284d3417d7d19c28a4982e3ee
Karra <karra.maynard@gmail.com>
2023-12-07 07:01:03 -0500

    grab user data during get route

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const {User, Boardgame} = require('../models');
 // Get routes
 router.get('/', async (req, res) => {
   try {
@@ -15,7 +16,17 @@ router.get('/login', async (req, res) => {
 
 router.get('/inventory', async (req, res) => {
   try {
-    res.render('inventory');
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: {exclude: ['password']},
+      include: [{model: Boardgame}]
+    });
+
+    const user = userData.get({plain: true});
+
+    res.render('inventory', {
+      ...user,
+      logged_in: true
+    });
   } catch(err) {res.status(500).json(err)}});
 
 router.get('/form', async (req, res) => {
