9de7a5ec4107ac85018ac59dc62168e0dfb6f825
Karra <karra.maynard@gmail.com>
2023-12-06 23:20:13 -0500

    add comment

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Boardgame = require('./Boardgame');
 
+// Define User relationship to Boardgame
 User.hasMany(Boardgame, {foreignKey: 'user_id', onDelete: 'CASCADE'});
 Boardgame.belongsTo(User, {foreignKey: 'user_id'});
 
