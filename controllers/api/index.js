5d42ea2508a23903b1cb1d1a61ab170a54b3734f
Karra <karra.maynard@gmail.com>
2023-12-06 23:44:04 -0500

    add game-routes to api router

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const gameRoutes = require('./game-routes');
 
 router.use('/users', userRoutes);
+router.use('/boardgames', gameRoutes);
 
 module.exports = router;
