107d7ea78ef9864ab6b878e6e95642bdc3b23e9f
Karra <karra.maynard@gmail.com>
2023-12-06 23:11:17 -0500

    comment out old code

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,29 @@
 DROP DATABASE IF EXISTS boardgame_db;
 CREATE DATABASE boardgame_db;
 
-USE boardgame_db;
+-- USE boardgame_db;
 
-CREATE TABLE User (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  pass TEXT NOT NULL
-);
+-- CREATE TABLE User (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   username VARCHAR(50) NOT NULL,
+--   email VARCHAR(50) NOT NULL,
+--   pass TEXT NOT NULL
+-- );
 
-CREATE TABLE Boardgame (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(100) NOT NULL,
-  min INT NOT NULL,
-  max INT NOT NULL,
-  timeToPlay INT NOT NULL,
-  User_id INT,
-  FOREIGN KEY(User_id)
-  REFERENCES User(id)
-  ON DELETE SET NULL
-);
+-- CREATE TABLE Boardgame (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   title VARCHAR(100) NOT NULL,
+--   min INT NOT NULL,
+--   max INT NOT NULL,
+--   timeToPlay INT NOT NULL,
+--   User_id INT,
+--   FOREIGN KEY(User_id)
+--   REFERENCES User(id)
+--   ON DELETE SET NULL
+-- );
 
-INSERT INTO User (username, email, pass)
-VALUES ('Julian', 'jbear97@email.com', 'password1234');
+-- INSERT INTO User (username, email, pass)
+-- VALUES ('Julian', 'jbear97@email.com', 'password1234');
 
-INSERT INTO Boardgame (title, min, max, timeToPlay)
-VALUES ('sampletitle', 2, 6, 20);
+-- INSERT INTO Boardgame (title, min, max, timeToPlay)
+-- VALUES ('sampletitle', 2, 6, 20);
