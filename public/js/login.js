85bad1089dc7e2e995a6fc770dc4f53aa528d322
Karra <karra.maynard@gmail.com>
2023-12-07 05:48:49 -0500

    fix get routes

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,8 @@
 const loginHandler = async (event) => {
   event.preventDefault();
 
-  const email = document.getElementById('email-login');
-  const pass = document.getElementById('pass-login');
+  const email = document.getElementById('email-login').value;
+  const pass = document.getElementById('pass-login').value;
 
   if (email && pass) {
     const response = await fetch('/api/users/login', {
@@ -10,9 +10,9 @@ const loginHandler = async (event) => {
       body: JSON.stringify({email, pass}),
       headers: {'Content-Type': 'application/json'}
     });
-    
+
     if (response.ok) {document.location.replace('/inventory');
     } else {alert(response.statusText);}
   }};
 
-document.querySelector('.login-form').addEventListener('submit', loginHandler);
+document.getElementById('login-form').addEventListener('click', loginHandler);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,19 +1,19 @@
 const signupHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.getElementById('name-signup');
-  const email = document.getElementById('email-signup');
-  const pass = document.getElementById('pass-signup');
+  const username = document.getElementById('name-signup').value;
+  const email = document.getElementById('email-signup').value;
+  const pass = document.getElementById('pass-signup').value;
 
   if (username && email && pass) {
-    const response = await fetch('/api/users', {
+    const response = await fetch('/api/users/form', {
       method: 'POST',
       body: JSON.stringify({username, email, pass}),
-      headers: {'Content-Type:': 'application/json'}
+      headers: {'Content-Type': 'application/json'}
     });
     
     if (response.ok) {document.location.replace('/inventory');
     } else {alert(response.statusText);}
 }};
 
-document.querySelector('.signup-form').addEventListener('submit', signupHandler);
+document.getElementById('signup-form').addEventListener('click', signupHandler);
