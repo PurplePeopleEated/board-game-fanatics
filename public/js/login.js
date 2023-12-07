80815d2acb8779ec9fabab74106909df1745359c
Karra <karra.maynard@gmail.com>
2023-12-06 19:31:58 -0500

    format

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,37 @@
-loginHandler = /*async*/ (event) => {
+const signupHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.getElementById('name-input');
-  const email = document.getElementById('email-input');
-  const pass = document.getElementById('pass-input');
-}
+  const name = document.getElementById('name-signup');
+  const email = document.getElementById('email-signup');
+  const pass = document.getElementById('pass-signup');
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({name, email, pass}),
+      headers: {'Content-Type:': 'signup/json'}
+    });
+    
+    if (response.ok) {document.location.replace('/inventory');}
+    else {alert(response.statusText);}
+}};
+
+const loginHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById('email-login');
+  const pass = document.getElementById('pass-login');
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({email, pass}),
+      headers: {'Content-Type': 'signup/json'}
+    });
+    
+    if (response.ok) {document.location.replace('/inventory');}
+    else {alert(response.statusText);}
+  }};
+
+document.querySelector('.signup-form').addEventListener('submit', signupHandler);
+document.querySelector('.login-form').addEventListener('submit', loginHandler);
